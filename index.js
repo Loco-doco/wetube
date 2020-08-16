a47dfae645aba59ca41b1fd4630372c3fd4c103e
Loco-doco <55223051+Loco-doco@users.noreply.github.com>
2020-08-16 18:01:11 +0900

    json setting, middleware

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+const app = express()
+
+const PORT = 4000;
+
+const handleListening = () => {
+    console.log(`Listening on : http://localhost:${PORT}`)
+}
+
+const handleHome = (req, res) => {
+    res.send('Hello from home')
+}
+
+const handleProfile = (req,res) => {
+    res.send("Hello From Profile")
+}
+
+app.use(cookieParser());
+
+app.unsubscribe(bodyParser.json());
+app.unsubscribe(bodyParser.urlencoded({extended: true}))
+app.use(helmet());
+app.use(morgan("tiny"));
+
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
