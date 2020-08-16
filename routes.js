5696b3b8dd41be33f2023387557ea8419665fa92
Loco-doco <55223051+Loco-doco@users.noreply.github.com>
2020-08-16 18:34:50 +0900

    routes URLs

---
+++ routes.js
@@ -0,0 +1,10 @@
+// Global
+const HOME = "/";
+const JOIN = "/join";
+const LOGIN = "/login";
+const LOGOUT = "/logout";
+const SEARCH = "/search";
+
+// Users
+const USERS = "/users";
+const USER_DETAIL = "/"
