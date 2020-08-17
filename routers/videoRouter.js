2981f6118bc39ff71360cfdcffba7eeee81af0d7
Loco-doco <55223051+Loco-doco@users.noreply.github.com>
2020-08-17 15:55:57 +0900

    routes URLs && seperated files

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,13 @@
 import express from "express"
+import routes from "../routes"
 
 const videoRouter = express.Router();
+
+videoRouter.get(routes.videos, (req,res) => res.send("videos"))
+videoRouter.get(routes.upload, (req,res) => res.send("upload"))
+videoRouter.get(routes.videoDetail, (req,res) => res.send("video_detail"))
+videoRouter.get(routes.editVideo, (req,res) => res.send("edit_video"))
+videoRouter.get(routes.deleteVideo, (req,res) => res.send("delete_video"))
+
 
 export default videoRouter;
